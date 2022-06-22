deba00f396824642fac8f24c7e0fda1247934f94
Alessandro <alembfilho@gmail.com>
2022-06-22 00:16:14 -0300

    dynamic

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle } from "phosphor-react";
+import { CheckCircle, Lock } from "phosphor-react";
 
 export interface LessonInterface {
     id: string;
@@ -9,16 +9,24 @@ export interface LessonInterface {
 }
 
 export default function Lesson({ data: lesson }: { data: LessonInterface }) {
+    const isAvailable = false
     return (
         <a href="">
             <span className="text-zinc-400">Domingo • 20 de junho • 19h00</span>
             <div className="border rounded border-zinc-600 p-4 mt-2">
                 <div className="flex justify-between mb-4">
-                    <span className="flex items-center gap-2 text-green-300 text-sm">
-                        <CheckCircle size={18} />
-                        Conteúdo liberado
+                    {isAvailable ?
+                        (<span className="flex items-center gap-2 text-green-300 text-sm">
+                            <CheckCircle size={18} />
+                            Conteúdo liberado
+                        </span>) :
+                        (<span className="flex items-center gap-2 text-orange-300 text-sm">
+                            <Lock size={18} />
+                            Em breve
+                        </span>)}
+                    <span className=" text-xs font-semibold border border-green-500 rounded px-1">
+                        {lesson.lessonType == 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
-                    <span className="text-green-500 text-xs font-semibold border border-green-500 rounded px-1">AO VIVO</span>
                 </div>
 
                 <strong>{lesson.title}</strong>
