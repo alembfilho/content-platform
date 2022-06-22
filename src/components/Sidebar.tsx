7ee3532883a0b0665f7ba9ef900b5630f9aac49c
Alessandro <alembfilho@gmail.com>
2022-06-21 22:34:02 -0300

    height sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import Class from "./Class";
 
 export default function Sidebar() {
     return (
-        <div className="w-[348px] h-full min-h-screen bg-zinc-800 border-l-zinc-600 border-l-[1px]">
+        <div className="w-[348px] min-h-screen bg-zinc-800 border-l-zinc-600 border-l-[1px]">
             <h1>Cronograma das aulas</h1>
             <Class />
         </div>
