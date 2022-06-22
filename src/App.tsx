8eeb54ec970737219486289534c70873fc2f4855
Alessandro <alembfilho@gmail.com>
2022-06-21 21:06:03 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import { gql, useQuery } from "@apollo/client"
+import Sidebar from "./components/sidebar"
+import Video from "./components/Video"
 
-const GET_LESSONS = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
+// const GET_LESSONS = gql`
+//   query{
+//     lessons{
+//       id
+//       title
+//     }
+//   }
+// `
+// interface Lesson {
+//   id: string;
+//   title: string;
+// }
+
+// const { data } = useQuery(GET_LESSONS)
+// console.log(data)
 
 function App() {
-  const { data } = useQuery(GET_LESSONS)
-  console.log(data)
 
-  return (
-    <ul>
-      {data?.lessons.map((lesson: Lesson) => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
-  )
+
+  return (<>
+    <header className="flex w100 items-center justify-center h-[72px] gap-2 border-b-gray-600 border-b-[1px]">
+      <h1 className="text-4xl font-semibold">ignite lab</h1>
+      <div className="w-1 h-8 bg-green-400 mt-2"></div>
+      <span className="mt-2 tracking-widest text-gray-200 ">REACTJS</span>
+    </header>
+    <Video />
+    <Sidebar />
+
+  </>)
 }
 
 export default App
