6ad17cb866def94042021efa2646884f9f1289dc
Alessandro <alembfilho@gmail.com>
2022-06-22 23:28:39 -0300

    responsive menu

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -4,18 +4,20 @@ import Video from "./Video";
 
 export default function Event() {
     const { slug } = useParams<{ slug: string }>()
+
     return (
         <>
             <header className="flex w100 items-center justify-center h-[72px] gap-2 bg-zinc-800 border-b-zinc-600 border-b-[1px]">
                 <h1 className="text-4xl font-semibold">ignite lab</h1>
                 <div className="w-1 h-8 bg-green-400 mt-2"></div>
                 <span className="mt-2 tracking-widest text-gray-200 ">REACTJS</span>
+
             </header>
+
             <main className="flex">
                 <Video slug={slug} />
-                {/* <Sidebar slug={slug} /> */}
+                <Sidebar slug={slug} />
             </main>
-
         </>
     )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
 import Lesson, { LessonInterface } from "./Lesson";
 import { gql, useQuery } from "@apollo/client"
+import { useEffect, useState } from "react";
+import { X, List } from "phosphor-react";
 
 
 const GET_LESSONS = gql`
@@ -20,8 +22,28 @@ interface GetLessonsResponse {
 
 export default function Sidebar({ slug }: { slug?: string }) {
   const { data } = useQuery<GetLessonsResponse>(GET_LESSONS)
-  return (
-    <aside className="w-[348px] min-h-screen flex flex-col gap-6 p-6 bg-zinc-800 border-l-zinc-600 border-l">
+
+  const [width, setWidth] = useState<number>(window.innerWidth)
+  const [isOpen, toggleMenu] = useState(true)
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWidth(window.innerWidth))
+
+    return () => window.removeEventListener('resize', () => setWidth(window.innerWidth))
+  }, []);
+
+  const isMobile = width <= 1024;
+
+  return (<>
+    <button
+      onClick={() => toggleMenu(old => !old)}
+      className="m-6 h-18 fixed right-0 top-0 text-2xl lg:hidden"
+    >
+      {isOpen ? <X /> : <List />}
+    </button>
+
+    <aside className={"absolute lg:relative lg:max-w-[348px] min-w-[100vw] lg:min-w-[310px] min-h-screen flex flex-col flex-1 gap-6 p-6 bg-zinc-800 border-l-zinc-600 lg:border-l"
+      + (!isOpen && isMobile ? " hidden" : "")}>
       <h1 className="text-2xl font-semibold pb-6 border-b border-zinc-600">Cronograma das aulas</h1>
 
       {data?.lessons.map(lesson => (
@@ -32,5 +54,8 @@ export default function Sidebar({ slug }: { slug?: string }) {
       ))}
 
     </aside>
+  </>
+
   )
+
 }
